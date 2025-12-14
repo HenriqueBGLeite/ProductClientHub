d122f9f45296d3a4f6b40c3a9830ccf6241864ad
HenriqueBGLeite <henriquecebrac37@gmail.com>
2025-12-14 20:34:37 -0300

    feat: tratando exception com filter na API

--- ProductClientHub.API/Program.cs
+++ ProductClientHub.API/Program.cs
@@ -1,3 +1,5 @@
+using ProductClientHub.API.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,21 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Services Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(option => option.Filters.Add(typeof(ExceptionFilter)));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
