628bb5b867cbf24a9d2a14b3a0e4678387cc7a28
HenriqueBGLeite <henriquecebrac37@gmail.com>
2025-12-15 10:27:28 -0300

    feat: editando dados de um cliente

--- ProductClientHub.API/Controllers/ClientsController.cs
+++ ProductClientHub.API/Controllers/ClientsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.API.UseCases.Clients.GetAll;
 using ProductClientHub.API.UseCases.Clients.Register;
+using ProductClientHub.API.UseCases.Clients.Update;
 using ProductClientHub.Communication.Requests;
 using ProductClientHub.Communication.Responses;
 
@@ -10,7 +12,7 @@ namespace ProductClientHub.API.Controllers;
 public class ClientsController : ControllerBase
 {
     [HttpPost]
-    [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseShortClientJson), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestClientJson request)
     {
@@ -22,15 +24,31 @@ public class ClientsController : ControllerBase
     }
 
     [HttpPut]
-    public IActionResult Update()
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
+    public IActionResult Update([FromRoute] Guid id, [FromBody] RequestClientJson request)
     {
-        return Ok();
+        var useCase = new UpdateClientUseCase();
+
+        useCase.Execute(id, request);
+
+        return NoContent();
     }
 
     [HttpGet]
+    [ProducesResponseType(typeof(ResponseAllClientsJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult GetAll()
     {
-        return Ok();
+        var useCase = new GetAllClientsUseCase();
+
+        var response = useCase.Execute();
+
+        if (response.Clients.Count == 0)
+            return NoContent();
+        
+        return Ok(response);
     }
 
     [HttpGet]
---
+++ ProductClientHub.API/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductClientHub.API.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public string Name { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public Guid ClientId { get; set; }
+}
